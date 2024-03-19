e76a0035ca0183d321307afe9e8a88e216d6169a
Nicholas Payk <nicholas_b_01@hotmail.com>
2024-03-19 14:47:12 +1100

    Fix: Widgets on Mobile Devices

--- Client/Frontend/src/App.jsx
+++ Client/Frontend/src/App.jsx
@@ -9,10 +9,38 @@ import './index.css'
 function App() {
 
   return (
-    <div className={"grid grid-rows-responsive grid-cols-6 h-screen"}>
-      <nav className='grid-rows-1 col-span-6 shrink-0'></nav>
-      <aside className='grid-rows-2 row-span-6 grid-cols-1 col-span-1'></aside>
-      <main className='grid-rows-2 grid-col-2'></main>
+    <div className={"flex flex-col h-screen"}>
+      <nav className="flex-none h-16 bg-black "></nav>
+      <main className='flex-grow flex md:flex-row flex-col bg-gradient-to-r from-purple-500 to-orange-500'>
+        <aside className='lg:w-64 md:w-40 md:h-full md:pr-0 sm:w-full h-20 flex px-4 py-4'>
+          <div className='bg-white flex-grow rounded-md'>
+
+          </div>
+        </aside>
+        <div className='md:grid md:grid-cols-2 md:grid-rows-2 flex overflow-hidden w-full flex-grow gap-4 p-4'>
+          <div className='col-auto row-auto flex'>
+            <iframe src="https://tailwindcss.com/docs/background-color" 
+                    frameborder="0"
+                    className='flex-grow rounded-md'></iframe>
+          </div>
+          <div className='col-auto row-auto flex w-full'>
+            <iframe src="https://tailwindcss.com/docs/background-color" 
+                    frameborder="0"
+                    className='flex-grow rounded-md'></iframe>
+          </div>
+          <div className='col-auto row-auto flex '>
+            <iframe src="https://tailwindcss.com/docs/background-color" 
+                    frameborder="0"
+                    className='flex-grow rounded-md'></iframe>
+          </div>
+          <div className='col-auto row-auto flex '>
+            <iframe src="https://tailwindcss.com/docs/background-color" 
+                    frameborder="0"
+                    className='flex-grow rounded-md'></iframe>
+          </div>
+        </div>
+
+      </main>
 
     </div>
   )
