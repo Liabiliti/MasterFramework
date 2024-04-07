9274e7973209aaf822b5c694072cb6c867d83bb9
Nicholas Payk <nicholas_b_01@hotmail.com>
2024-04-07 11:21:46 +1000

    Changed DB Structure

--- server/server.js
+++ server/server.js
@@ -10,11 +10,10 @@ app.use(cors({origin: 'http://localhost:5173', credentials: true}));
 app.use(express.json());
 
 app.get("/api/user", (req, res) => {
-    const query = "SELECT UserID FROM users WHERE emailAddress = ? AND password = ?";
+    const query = "SELECT ID FROM admin WHERE `User Name` = ? AND Password = ?";
     db.query(query, [req.query.email, req.query.password], (error, results) => {
    
         if(error){
-            // console.log(error);
             throw error;
         }
         res.json(results);
