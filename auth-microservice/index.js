023fcb5a0319073a4ecfa6ae9df5cfeb634fff50
Nicholas Payk <nicholas_b_01@hotmail.com>
2024-04-23 10:56:36 +1000

    Argon2id Implemented

--- auth-microservice/index.js
+++ auth-microservice/index.js
@@ -11,6 +11,21 @@ const LocalStrategy = require("passport-local").Strategy;
 // Import Database for querying
 const db = require("./configs/db")
 
+// JSON Web Token
+const jwt = require("jsonwebtoken")
+
+// Hashing algorithm
+const argon2 = require("argon2");
+const crypto = require('crypto');
+
+const hashingConfig = {
+    type: argon2.argon2id,
+    memoryCost: 20000,
+    timeCost: 2, // 2 iterations
+    parallelism: 1, // 1 degree of parallelism
+    saltLength: 16, // Length of the salt
+};
+
 // Initialise the Middleware
 // This is the basic express session({...}) for inisitalisation
 app.use(session({
@@ -25,21 +40,68 @@ app.use(passport.initialize());
 // Enables Express's Session management
 app.use(passport.session());
 
-const authUser = (username, password, done) => {
-    const query = "SELECT ID FROM admin WHERE `User Name` = ? AND Password = ?";
-    db.query(query, [username, password], (error, results) => {
-        if (error) {
-            throw error;
+async function hashPassword(password) {
+    const salt = crypto.randomBytes(hashingConfig.saltLength);
+    // console.log("\n", salt);
+    try {
+        const hash = await argon2.hash(password, {
+            ...hashingConfig,
+            salt: salt,
+        });
+        console.log("Hashed password:", typeof(hash));
+        return hash;
+    } catch (err) {
+        console.error("Error hashing password:", err);
+    }
+}
+
+async function verifyPassword(hash, password) {
+    try {
+        if (await argon2.verify(hash, password)) {
+            console.log("Password is correct.");
+            return true;
+        } else {
+            console.log("Password is incorrect.");
+            return false;
         }
-        // console.log("Made it to there")
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const authUser = async (username, password, done) => {
+    try {
+        // console.log(hash);
+        const query = "SELECT ID, Password FROM admin WHERE `User Name` = ?";
+
+        const results = await new Promise((resolve, reject) => {
+            db.query(query, [username], (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve(results);
+                }
+            });
+        });
+
         if (results.length > 0) {
-            console.log("Made it to there")
-            let { ID } = results[0];
-            return done(null, ID)
+            // const userPassword = await hashPassword(results[0].Password)
+            const isPasswordCorrect = await verifyPassword(results[0].Password, password);
+            if (isPasswordCorrect) {
+                const token = jwt.sign({ id: results[0], username: username }, "your_secret_key", { expiresIn: "1h" });
+                console.log("Made it to there")
+                return done(null, { id: results[0], token: token })
+            }
         }
         console.log("Unsuccessful")
-        return (null, false)
-    });
+        return done(null, false)
+
+    }
+    catch (err) {
+        console.log("Error in authenticating user: ", err)
+        return done(err);
+    }
 }
 
 // The authUser function will contain the steps required to authenticate a user
@@ -52,7 +114,7 @@ passport.serializeUser((userID, done) => {
 
 passport.deserializeUser((userID, done) => {
     console.log(" ------> Deserialise")
-    done(null, {ID: userID} )
+    done(null, { ID: userID })
 })
 
 const cors = require("cors");
@@ -63,12 +125,11 @@ const port = 3001;
 app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173'], credentials: true }));
 app.use(express.json());
 
-app.post("/login", passport.authenticate("local"), (req,res) => {
+app.post("/login", passport.authenticate("local"), (req, res) => {
     // let userClearance = req.user;
-    console.log(req.isAuthenticated())
-    console.log(req.user)
-    res.json({ message : "Access"})
-})  
+    res.cookie("token", req.user.token, { httpOnly: true, maxAge: 3600000 })
+    // res.json({message: req.user.token})
+})
 
 
 app.get("/example", (req, res) => {
@@ -86,7 +147,7 @@ app.get("/", (req, res) => {
 
 
 app.post("/logout", (req, res, next) => {
-    req.logout(function(err) {
+    req.logout(function (err) {
         if (err) { return next(err); }
         console.log(req.isAuthenticated())
         res.json({ message: "Logged out successfully" });
